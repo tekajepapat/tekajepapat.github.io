30b62660b9e3c04ec8639df593c6060f79572c90
tekajepapat <155725043+tekajepapat@users.noreply.github.com>
2024-04-05 07:19:29 +0700

    Update slide.js

--- slide.js
+++ slide.js
@@ -2,13 +2,14 @@ const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: "infinite",
 autoplay: {
-    delay: 2000, // delay in milliseconds between slides
-    disableOnInteraction: false, // prevent autoplay from being stopped when user interacts with swiper
-  },
+    delay: 2000, 
+    disableOnInteraction: false, 
+},
   // Pagination
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
 
   // Navigation arrows
