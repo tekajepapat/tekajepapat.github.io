798601bd8ac800fdde39c8549d1891c469c3c094
tekajepapat <155725043+tekajepapat@users.noreply.github.com>
2024-04-07 06:57:07 +0700

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,10 +1,12 @@
+// Autoplay
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
-  loop: "infinite",
-autoplay: {
-    delay: 2000, 
-    disableOnInteraction: false, 
-},
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+
   // Pagination
   pagination: {
     el: ".swiper-pagination",
@@ -16,5 +18,25 @@ autoplay: {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-  },
+  }
 });
+
+// Open Modal
+$(document).ready(function() {
+  $('.swiper-slide').on('click', function() {
+    var imgSrc = $(this).css('background-image').replace('url("', '').replace('")', '');
+    $('#modalImg').attr('src', imgSrc);  
+    $('#myModal').css("display", "block"); 
+  });
+
+  $(document).on('click', function(event) {
+    if (event.target == document.getElementById("myModal")) {
+      $('#myModal').css("display", "none"); 
+    }
+  });
+
+  $('.close').on('click', function() {
+    $('#myModal').css("display", "none"); 
+  });
+});
+
